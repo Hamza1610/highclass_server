060b67e0bf85918e9e633e41348c3fac370af84f
Hamza1060 <muhammadhamza162003@gmail.com>
2023-10-03 23:20:35 +0100

    added routes

--- server.js
+++ server.js
@@ -4,11 +4,43 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 
-app.listen(4000, () => {
-	console.log('Server runnign on port 4000! ');
-}
-)
+app.listen(4000, () => console.log('Server runnign on port 4000! '))
 
-app.get('/', (req, res) => {
+// PUSH request for Sign in
+app.get('/signin', (req, res) => {
 	console.log('GET request placed');
-})
+});
+
+// PUSH request for Log in
+app.get('/login', (req, res) => {
+        console.log('GET request placed');
+});
+
+// PUSH request for registering courses 
+app.get('/register-courses', (req, res) => {
+        console.log(res);
+});
+
+// PUSH request for study timetable
+app.get('/register-timetable', (req, res) => {
+        console.log('GET request placed');
+});
+// GET request for Landing page/Home
+app.get('/', (req, res) => {
+	        console.log('GET request placed');
+});
+
+// GET request for Collaborative Learning
+app.get('/collaborative-learning', (req, res) => {
+        console.log('GET request placed');
+});
+
+// GET request for Quiz and Rewards
+app.get('/quiz-and-reward', (req, res) => {
+        console.log('GET request placed');
+});
+
+// GET request for Reading scheduler
+app.get('/reading-scheduler', (req, res) => {
+        console.log('GET request placed');
+});
